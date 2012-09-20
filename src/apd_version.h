3ddc81e49f23601587b7738ea46c82f49fd1b6ac
Gyuri Schreiber <gy.schreiber@apduino.org>
2012-09-20 18:02:58 +0200

    refs #21, refs #22, refs #44, refs #45 : bump version

--- src/apd_version.h
+++ src/apd_version.h
@@ -26,7 +26,7 @@
 #ifndef APD_VERSION_H_
 #define APD_VERSION_H_
 
-#define APDUINO_VERSION    "0.55"
-#define APDUINO_BUILD      "2012081001h"
+#define APDUINO_VERSION    "0.56"
+#define APDUINO_BUILD      "2012092001h"
 
 #endif /* APD_VERSION_H_ */
